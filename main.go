c23dc659627e0430a22b3b26b72700e0408439da
connor41 <primerovmax@gmail.com>
2018-08-03 13:47:59 +0300

    Add write form

--- main.go
+++ main.go
@@ -15,11 +15,21 @@ func indexHadler(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "index", nil)
 }
 
+func writeHadler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "write", nil)
+}
+
 func main() {
 	fmt.Println("Listening on port :3000")
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 	http.HandleFunc("/", indexHadler)
+	http.HandleFunc("/write", writeHadler)
 
 	http.ListenAndServe(":3000", nil)
 }
