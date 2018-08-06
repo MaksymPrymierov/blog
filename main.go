135134ab44c4ceb19262e9484dbef935ae02ee43
connor41 <primerovmax@gmail.com>
2018-08-06 17:18:17 +0300

    Add edit post and reading post

--- main.go
+++ main.go
@@ -5,14 +5,17 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 var posts map[string]*modules.Post
+var postsID int
 
 func indexHadler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
+		return
 	}
 	t.ExecuteTemplate(w, "index", posts)
 }
@@ -21,6 +24,7 @@ func writeHadler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
+		return
 	}
 
 	t.ExecuteTemplate(w, "write", nil)
@@ -37,22 +41,61 @@ func createPostHadler(w http.ResponseWriter, r *http.Request) {
 		post.Title = title
 		post.Text = text
 	} else {
+		id = strconv.Itoa(postsID)
 		post := modules.NewPost(id, title, text)
 		posts[post.Id] = post
 	}
 
+	postsID = postsID + 1
 	http.Redirect(w, r, "/", 302)
+}
+
+func editPostHadler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	id := r.FormValue("id")
+	post, found := posts[id]
+	if !found {
+		http.NotFound(w, r)
+		return
+	}
+
+	t.ExecuteTemplate(w, "write", post)
+}
+
+func readPostHadler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/read.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	id := r.FormValue("id")
+	post, found := posts[id]
+	if !found {
+		http.NotFound(w, r)
+		return
+	}
+
+	t.ExecuteTemplate(w, "read", post)
 }
 
 func main() {
 	fmt.Println("Listening on port :3000")
 
 	posts = make(map[string]*modules.Post, 0)
+	postsID = 1
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 	http.HandleFunc("/", indexHadler)
 	http.HandleFunc("/write", writeHadler)
 	http.HandleFunc("/createPost", createPostHadler)
+	http.HandleFunc("/editPost", editPostHadler)
+	http.HandleFunc("/readPost", readPostHadler)
 
 	http.ListenAndServe(":3000", nil)
 }
