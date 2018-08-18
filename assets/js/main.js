715f55e73335be3fae8c791f7ceec883748fafcd
ya-alexandr-86 <ya.alexandr.86@gmail.com>
2018-08-18 18:16:34 +0300

    take a vlidation on registration form

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+
+
+function regexpCheck(expration, testedString) {
+  return expration.test(testedString);
+}
+
+$(document).ready(function() { 
+    
+    let regMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let regName = /^[a-z0-9_-]{3,20}$/;
+    let regPass = /^[a-z0-9_-]{3,60}$/;
+    let userIP = "";
+
+
+    $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
+      userIP = JSON.stringify(data.ip, null, 2).substr(1).slice(0, -1);
+      $("#ip").val(userIP);
+      console.log(userIP);//test ip
+    });
+
+
+    /*проверка email*/
+    $('#email, #username, #password').on('input', function() {
+      validEmail = regexpCheck(regMail, $('#email').val());
+      validName = regexpCheck(regName, $('#username').val());
+      validPass = regexpCheck(regPass, $('#password').val());
+      
+      if(validEmail && validName && validPass) {
+        $('#btn-submit').prop('disabled', false);
+      }
+      else {
+        $('#btn-submit').prop('disabled', true);
+      }
+    });
+    
+});
